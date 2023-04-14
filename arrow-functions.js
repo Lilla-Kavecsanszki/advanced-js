fa0b3f6e15f38456a3b956b1181e06854dfdc679
Lilla-Kavecsanszki <>
2023-04-14 13:46:19 +0000

    arrow functions JS

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,45 @@
 
 // Vanilla JavaScript Function
 
+function addTwoNumbers(a,b) {
+    //code block
+    return a + b;
+}
+
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
+
 // Arrow Function With Parameters
+
+const addTwoNumbers = (a, b) => {
+    //code block
+    return a + b;
+}
+
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
 
+const addTwoNumbers2 = (a, b) => a + b;
+// const addTwoNumbers = (a, b) => (a + b); also valid
+
+let sum2 = addTwoNumbers2(6, 4);
+console.log(sum2);
+
 // Implicit Returns
 
-// Returning Multiple Lines
+const saySomething = message => console.log(message);
+saySomething('Hello there!!');
+
+const sayHello = () => console.log('hello');
+sayHello();
+
+// Returning Multiple Lines - the parentheses!!!! also when i call them 
+
+const returnMultipleLines = () => (
+    `<p>
+      This is a multiline string!
+    </p>`
+)
+console.log(returnMultipleLines());
